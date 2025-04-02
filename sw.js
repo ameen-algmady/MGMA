69daef04d0202e0ec790ce6586d8b744bbb01934
ameen-algmady <ameengmady1982@gmail.com>
2025-04-03 02:00:23 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,25 @@
-const CACHE_NAME = 'mgma-cache-v1';
+const CACHE_NAME = 'mgma-v4';
 const urlsToCache = [
   '/MGMA/',
   '/MGMA/index.html',
-  '/MGMA/manifest.json',
-  '/MGMA/icons/icon-192x192.png',
-  '/MGMA/icons/icon-512x512.png'
+  '/MGMA/styles.css',
+  '/MGMA/school-logo.png',
+  '/MGMA/school-image.jpg',
+  '/MGMA/ãÏíÑ-ÇáãÌãÚ.jpg',
+  '/MGMA/ÑíÇÖ-ÇáÌæÏÉ.jpg',
+  '/MGMA/manifest.json'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((response) => response || fetch(event.request))
   );
 });
